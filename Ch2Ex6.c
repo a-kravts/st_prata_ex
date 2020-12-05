ccb3ab5dcaa900d2bf8ab24b73593f69c1e38f63
Alexandr Kravtsov <kr-al-ko@mail.ru>
2020-12-06 01:20:50 +0300

    added Ch2Ex6.c

---
+++ Ch2Ex6.c
@@ -0,0 +1,17 @@
+/* print toes = 10, toes*2, toes*toes
+   filename:Ch2Ex6.c
+   ver 1.0
+   Alexandr Kravtsov */
+
+#include <stdio.h>
+
+int main()
+{
+	int toes = 10;
+	
+	printf("toes = %d.\n"
+		   "toes*2 = %d.\n"
+		   "toes*toes = %d.\n", toes, toes*2, toes*toes);
+	
+	return 0;
+}
