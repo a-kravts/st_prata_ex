9ff21741824a17a3e394fbfa4905ad0ff594e2dc
Alexandr Kravtsov <kr-al-ko@mail.ru>
2020-12-05 23:14:08 +0300

    added Ch2Ex1

---
+++ Ch2Ex1.c
@@ -0,0 +1,16 @@
+/* output first and last name
+   filename: Ch2Ex1.c
+   ver 1.0
+   Alexandr Kravtsov */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Alexandr Kravtsov\n");
+	printf("Alexandr\nKravtsov\n");
+	printf("Alexandr ");
+	printf("Kravtsov\n");
+	
+	return 0;
+}
