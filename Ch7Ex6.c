3b3c0c97c51526d8012c84d2265a89294c120050
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-02-04 08:53:25 +0300

    changed Ch7Ex6.c

--- Ch7Ex6.c
+++ Ch7Ex6.c
@@ -1,6 +1,6 @@
 /* Counting the number of occurrences of the sequence "ei"
    filename: Ch7Ex6.c
-   ver 1.0
+   ver 2.0
    Alexandr Kravtsov */
 
 #include <stdio.h>
@@ -16,19 +16,16 @@ int main()
 	
 	printf("Enter any characters('#' to exit):\n");
 	while((current_ch = getchar()) != STOP) {
-		if(current_ch == 'e')
+		if(current_ch == 'e') {
 			is_e = true;
-		else if(current_ch == 'i' && is_e) {
-			number_of_occurrences_ei++;
-			is_e = false;
+			continue;
 		}
-		else
-			is_e = false;
+		else if(current_ch == 'i' && is_e)
+			number_of_occurrences_ei++;
+		is_e = false;
 	}
 	printf("\nNumber of occurrences of the sequence \"ei\" = %d\n", 
 			number_of_occurrences_ei);
 	
-	
 	return 0;
 }
-
