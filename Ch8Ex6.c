3e1c486760606031aba59fb3eefdbb6345f1173a
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-02-19 03:15:04 +0300

    fixed Ch8Ex6.c

--- Ch8Ex6.c
+++ Ch8Ex6.c
@@ -1,10 +1,9 @@
 /* Check a function that returns the first non-whitespace character
    filename: Ch8Ex6.c
-   ver 1.0
+   ver 2.0
    Alexandr Kravtsov */
 
 #include <stdio.h>
-#include <stdbool.h>
 #include <ctype.h>
 
 char get_first_non_space_char();
@@ -13,8 +12,8 @@ int main()
 {
 	int ch;
 	
-	printf("Enter any characters('q' to exit):\n");
-	while((ch = get_first_non_space_char()) != 'q')
+	printf("Enter any non-whitespace characters('q' to exit):\n");
+	while((ch = get_first_non_space_char()) != 'q') 
 		printf("%c\n", ch);
 	
 	return 0;
@@ -22,13 +21,15 @@ int main()
 
 char get_first_non_space_char()
 {
-	int ch, non_space;
-	bool have_the_first = false;
-	while((ch = getchar()) != '\n') {
-		if(!isspace(ch) && !have_the_first) {
-			non_space = ch;
-			have_the_first = true;
+	int ch;
+	while(1) {
+		ch = getchar();
+		if(!isspace(ch)) {
+			while(getchar() != '\n')
+				continue;
+			return ch;
 		}
+		else
+			continue;
 	}
-	return non_space;
 }
