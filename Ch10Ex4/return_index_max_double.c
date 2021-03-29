7bc3abc9b65c2f9388df4bd4bff9ca75ece85562
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-03-29 08:52:09 +0300

    fixed return_index_max_double.c

--- Ch10Ex4/return_index_max_double.c
+++ Ch10Ex4/return_index_max_double.c
@@ -2,9 +2,9 @@
 
 int index_max_double(const double *arr, const double *end)
 {
-	double max = 0.0;
+	double max = *arr;
 	int index = 0, max_index = 0;
-	for(; arr < end; arr++, index++)
+	for(arr++; arr < end; arr++, index++)
 		if(*arr > max) {
 			max = *arr;
 			max_index = index;
