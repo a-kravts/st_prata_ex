6938db5cb96505fe2c148606bf9ba6e12dd3da06
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-03-29 16:01:22 +0300

    fixed reverse_array_of_double.c

--- Ch10Ex6/reverse_array_of_double.c
+++ Ch10Ex6/reverse_array_of_double.c
@@ -4,7 +4,7 @@ void reverse_array_of_double(double *arr, int quantity)
 {
 	double tmp;
 	int i;
-	for(i = 0; i <= (quantity / 2); i++) {
+	for(i = 0; i < (quantity / 2); i++) {
 		tmp = arr[i];
 		arr[i] = arr[quantity - i - 1];
 		arr[quantity - i - 1] = tmp;
