7ad04167b829316448779cc21dd860263b5af0e9
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-03-31 15:19:56 +0300

    added all files for Ch10Ex10

---
+++ Ch10Ex10/elementwise_sum_of_two_arrays_double.h
@@ -0,0 +1,4 @@
+/* Declarations for the elementwise_sum_of_two_arrays_double.c program */
+void elementwise_sum_double(double *dest, const double *sourse_1,
+							const double *source_2, int column);
+
