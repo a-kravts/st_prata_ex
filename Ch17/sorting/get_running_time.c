0ec5b53111eafce87d4854636ea9378ddd968916
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-11-04 15:13:17 +0300

    fixed get_running_time

--- Ch17/sorting/get_running_time.c
+++ Ch17/sorting/get_running_time.c
@@ -1,5 +1,6 @@
 #include "get_running_time.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
@@ -7,13 +8,15 @@
 double get_running_time(fp_t function, const int *array, int size)
 {
     clock_t start, finish;
+    int tmp[size];
 
+    memcpy(tmp, array, size * sizeof(int));
     start = clock();
     if(start == -1) {
         perror("clock");
         exit(EXIT_FAILURE);
     }
-    function(array, size);
+    function(tmp, size);
     finish = clock();
     if(finish == -1) {
         perror("clock");
--- Ch17/sorting/get_running_time.h
+++ Ch17/sorting/get_running_time.h
@@ -2,7 +2,7 @@
 #ifndef GET_RUNNING_TIME_H_SENTRY
 #define GET_RUNNING_TIME_H_SENTRY
 
-typedef void (*fp_t)(const int *, int); /*sort function pointer*/
+typedef void (*fp_t)(int *, int); /*sort function pointer*/
 
 extern double get_running_time(fp_t function, const int *array, int size);
 
