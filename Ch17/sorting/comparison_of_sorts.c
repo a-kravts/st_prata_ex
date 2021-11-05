e3cf213e04e445b251e8f266bea7f09a65a90708
Aleksandr Kravtsov <kr-al-ko@mail.ru>
2021-11-05 16:55:22 +0300

    changed comparison_of_sorts

--- Ch17/sorting/comparison_of_sorts.c
+++ Ch17/sorting/comparison_of_sorts.c
@@ -3,7 +3,7 @@
    ver 1.0
    Aleksandr Kravtsov */
 #include "bubble_sort.h"
-//#include "selection_sort.h"
+#include "selection_sort.h"
 //#include "insertion_sort.h"
 //#include "quick_sort.h"
 #include "get_running_time.h"
@@ -17,13 +17,13 @@ int main(void)
     int size = sizeof(array) / sizeof(*array);
 
     bubble_sort_time = get_running_time(bubble_sort, array, size);
-//    selection_sort_time = get_running_time(selection_sort, array, size);
+    selection_sort_time = get_running_time(selection_sort, array, size);
 //    insertion_sort_time = get_running_time(insertion_sort, array, size);
 //    quick_sort_time = get_running_time(quick_sort, array, size);
-    printf("bubble sort time    = %lf sec.\n", bubble_sort_time);
-//    printf("selection sort time = %lf sec.\n", selection_sort_time);
-//    printf("insertion sort time = %lf sec.\n", insertion_sort_time);
-//    printf("quick sort time     = %lf sec.\n", quick_sort_time);
+    printf("bubble sort time    = %.9lf sec.\n", bubble_sort_time);
+    printf("selection sort time = %.9lf sec.\n", selection_sort_time);
+//    printf("insertion sort time = %.9lf sec.\n", insertion_sort_time);
+//    printf("quick sort time     = %.9lf sec.\n", quick_sort_time);
 
     return 0;
 }
